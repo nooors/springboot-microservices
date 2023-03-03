70fe8eaeb0c1e9852b19a6a16929a5e17d207fd6
Jose.Nortes@gft.com <Jose.Nortes@gft.com>
2023-03-03 13:51:11 +0100

    Initial commit

---
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paymentchain.customer.controller;
+
+import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.respository.CustomerRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ *
+ * @author sotobotero
+ */
+@RestController
+@RequestMapping("/customer")
+public class CustomerRestController {
+
+    
+    @Autowired
+    CustomerRepository customerRepository;
+    
+    private final WebClient.Builder webClientBuilder;
+    
+    public CustomerRestController(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+    
+    @GetMapping()
+    public List<Customer> list() {
+        return customerRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Customer get(@PathVariable long id) {
+        return customerRepository.findById(id).get();
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable long id, @RequestBody Customer input) {
+         Customer find = customerRepository.findById(id).get();   
+        if(find != null){     
+            find.setCode(input.getCode());
+            find.setName(input.getName());
+            find.setIban(input.getIban());
+            find.setPhone(input.getPhone());
+            find.setSurname(input.getSurname());
+        }
+        Customer save = customerRepository.save(find);
+           return ResponseEntity.ok(save);
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Customer input) {
+        input.getProducts().forEach(x -> x.setCustomer(input));
+        Customer save = customerRepository.save(input);
+        return ResponseEntity.ok(save);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+          Optional<Customer> findById = customerRepository.findById(id);   
+        if(findById.get() != null){               
+                  customerRepository.delete(findById.get());  
+        }
+        return ResponseEntity.ok().build();
+    }
+    
+}
