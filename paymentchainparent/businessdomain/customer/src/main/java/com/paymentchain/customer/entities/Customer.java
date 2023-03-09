a885a529b3a7e374e997f283d8b92e9a265faed9
Jose.Nortes@gft.com <Jose.Nortes@gft.com>
2023-03-09 15:48:58 +0100

    payment chain is working

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -5,13 +5,16 @@
  */
 package com.paymentchain.customer.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 import lombok.Data;
@@ -26,15 +29,15 @@ public class Customer {
    @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
    private long id;
-   private String code;
    private String name;
-   private String phone;   
+    private String code;
     private String iban;
-    private String surname; 
-    private String address;     
+    private String names;
+    private String surname;
+    private String phone;
+    private String address; 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)   
     private List<CustomerProduct> products;
     @Transient
     private List<?> transactions;
-   
 }
